cae8f4f5657f150147deb0702298e8b753935ec2
Leandro Araújo <leandrocaaraujo@gmail.com>
2019-03-06 21:22:39 -0300

    Alter webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,5 +14,4 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
 
-mix.js('resources/js/customers/form_schedule_work.js', 'public/js')
-   .sass('resources/sass/customers/form_schedule_work.scss', 'public/css');;
+mix.js('resources/js/customers/form_schedule_work.js', 'public/js');
