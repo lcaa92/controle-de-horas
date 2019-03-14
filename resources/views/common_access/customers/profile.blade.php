d2bc036ecb86dd4e96710d3e1b17fea8672aa787
Leandro Araújo <leandrocaaraujo@gmail.com>
2019-03-14 00:11:40 -0300

    Alter absence box position and use main div with container-fluid

--- resources/views/common_access/customers/profile.blade.php
+++ resources/views/common_access/customers/profile.blade.php
@@ -22,7 +22,7 @@
 @endsection
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     @if (session('msg'))
         <div class="alert alert-{{session('alert')}}">
             {{ session('msg') }}
@@ -34,16 +34,33 @@
             <customer-profile-component :customer='{!! json_encode($customer) !!}'></customer-profile-component>
         </div>
         <div class="col-md">
-            <div class="card">
-                <div class="card-header">
-                    Escalas de trabalho
-                    <a class="float-right" href="{{ route('form.work.schedule', ['customer_id' => $customer->id]) }}"> Adicionar escala</a>
-                </div>
+            <div class="row" >
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            Escalas de trabalho
+                            <a class="float-right" href="{{ route('form.work.schedule', ['customer_id' => $customer->id]) }}"> Adicionar escala</a>
+                        </div>
 
-                <div class="card-body">
-                    <data-table fetch-url="{{ route('list.work.schedule', ['customer_id'=>$customer->id]) }}"> </data-table>
+                        <div class="card-body">
+                            <data-table fetch-url="{{ route('list.work.schedule', ['customer_id'=>$customer->id]) }}"> </data-table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-header">
+                            Abonos
+                            <a class="float-right" href="{{ route('form.absence.permission', ['customer_id' => $customer->id]) }}"> Adicionar abono</a>
+                        </div>
+
+                        <div class="card-body">
+                            <data-table fetch-url="{{ route('list.absence.permission', ['customer_id'=>$customer->id]) }}"> </data-table>
+                        </div>
+                    </div>
                 </div>
             </div>
+           
             <br />
 
             <div class="card">
@@ -59,16 +76,7 @@
 
             <br />
 
-            <div class="card">
-                <div class="card-header">
-                    Abonos
-                    <a class="float-right" href="{{ route('form.absence.permission', ['customer_id' => $customer->id]) }}"> Adicionar abono</a>
-                </div>
-
-                <div class="card-body">
-                    <data-table fetch-url="{{ route('list.absence.permission', ['customer_id'=>$customer->id]) }}"> </data-table>
-                </div>
-            </div>
+            
 
         </div>
     </div>
