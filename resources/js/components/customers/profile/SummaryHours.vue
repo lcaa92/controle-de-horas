10a626dac7f878a3f71889352cd7d66aebde334b
Leandro Araújo <leandrocaaraujo@gmail.com>
2019-03-18 23:56:14 -0300

    Component summary hours

---
+++ resources/js/components/customers/profile/SummaryHours.vue
@@ -0,0 +1,25 @@
+<template>
+    
+        <div class="card card-default">
+            <div class="card-header">Extrato de horas</div>
+
+            <div class="card-body">
+                
+                <data-table :fetch-url="fetchUrl"> </data-table>
+            </div>
+        </div>
+        
+</template>
+
+<script>
+    import DataTable from '../../DataTableComponent'
+    export default {
+        name: 'SummaryHours',
+        props:{
+            fetchUrl: { type: String, required: true }
+        },
+        mounted() {
+            console.log(this.fetchUrl)
+        }
+    }
+</script>
