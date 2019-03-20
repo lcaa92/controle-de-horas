fcd1d55662c68a7e7a109ee3de58e7c6dad55970
Leandro Araújo <leandrocaaraujo@gmail.com>
2019-03-20 20:43:20 -0300

    Color chart

--- resources/js/components/customers/profile/SummaryHours.vue
+++ resources/js/components/customers/profile/SummaryHours.vue
@@ -48,6 +48,8 @@
                             datasets: [{
                                 label: 'Diff',
                                 data: this.chartConfig.data,                        
+                                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                                borderColor: 'rgba(255, 99, 132, 1)',
                                 borderWidth: 1
                             }]
                         },
