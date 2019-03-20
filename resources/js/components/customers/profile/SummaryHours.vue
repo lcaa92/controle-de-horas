2510c72115398698bdbc0d3f6e9d46f674e4c5a0
Leandro Araújo <leandrocaaraujo@gmail.com>
2019-03-20 18:14:31 -0300

    Chart summary hours

--- resources/js/components/customers/profile/SummaryHours.vue
+++ resources/js/components/customers/profile/SummaryHours.vue
@@ -2,10 +2,10 @@
     
         <div class="card card-default">
             <div class="card-header">Extrato de horas</div>
-
             <div class="card-body">
-                
-                <data-table :fetch-url="fetchUrl"> </data-table>
+                <canvas id="myChart" width="400" height="400"></canvas>
+
+                <data-table ref="dataTableComponent" :fetch-url="fetchUrl"> </data-table>
             </div>
         </div>
         
@@ -13,13 +13,71 @@
 
 <script>
     import DataTable from '../../DataTableComponent'
+    import Chart from 'chart.js';
     export default {
         name: 'SummaryHours',
         props:{
-            fetchUrl: { type: String, required: true }
+            fetchUrl: { type: String, required: true },
+            chartUrlData: { type: String, required: true }
+        },
+        data: function () {
+            return {
+                chartConfig: {
+                    labels: [],
+                    data: [],
+
+                }
+            }
         },
         mounted() {
-            console.log(this.fetchUrl)
+            this.fetchChartData()
+        },
+        methods: {
+            fetchChartData() {
+                axios.get(this.chartUrlData)
+                .then((res) => {
+                    // console.log(res)
+                    let data = res.data.data
+                    this.chartConfig.data = data.map(el => el.diff_time_day)
+                    this.chartConfig.labels = data.map(el => el.date)
+                    console.log(this.chartConfig)
+                    this.createChart('myChart', {
+                        type: 'line',
+                        data: {
+                            labels: this.chartConfig.labels,
+                            datasets: [{
+                                label: 'Diff',
+                                data: this.chartConfig.data,                        
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                yAxes: [{
+                                    ticks: {
+                                        beginAtZero: true
+                                    }
+                                }]
+                            }
+                        }
+                    })
+                })
+                .catch((res) => {
+                    if(res instanceof Error) {
+                    console.log(res.message);
+                    } else {
+                    console.log(res.data);
+                    }
+                });
+            },
+            createChart(chartId, chartData) {
+                const ctx = document.getElementById(chartId);
+                const myChart = new Chart(ctx, {
+                    type: chartData.type,
+                    data: chartData.data,
+                    options: chartData.options,
+                });
+            }
         }
     }
 </script>
